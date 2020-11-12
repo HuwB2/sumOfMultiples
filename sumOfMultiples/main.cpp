f7bc74bb14cde1071106067fe6060cc76e2e3a51
Huw Borradaile <huw.borradaile@gmail.com>
2020-11-12 15:27:44 +0000

    Initial Commit

---
+++ sumOfMultiples/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sumOfMultiples
+//
+//  Created by Huw Borradaile on 12/11/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
